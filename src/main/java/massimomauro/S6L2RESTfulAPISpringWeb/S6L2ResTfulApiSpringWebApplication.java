9718ba93336c221b71c20a630ebf83095b561b22
MrMax01 <massimo.mauro00@gmail.com>
2023-11-07 16:01:30 +0100

    first commit

---
+++ src/main/java/massimomauro/S6L2RESTfulAPISpringWeb/S6L2ResTfulApiSpringWebApplication.java
@@ -0,0 +1,13 @@
+package massimomauro.S6L2RESTfulAPISpringWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S6L2ResTfulApiSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S6L2ResTfulApiSpringWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/massimomauro/S6L2RESTfulAPISpringWeb/S6L2ResTfulApiSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package massimomauro.S6L2RESTfulAPISpringWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S6L2ResTfulApiSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
