dc3c5b2fa5b7f473e7818dc5224e0a921e4aae4b
MrMax01 <massimo.mauro00@gmail.com>
2023-11-07 16:49:37 +0100

    blog controller

--- src/main/java/massimomauro/S6L2RESTfulAPISpringWeb/controllers/AuthorsController.java
+++ src/main/java/massimomauro/S6L2RESTfulAPISpringWeb/controllers/AuthorsController.java
@@ -8,36 +8,38 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/authors")
 public class AuthorsController {
     @Autowired
     private AuthorsService authorsService;
 
-    // 1. GET http://localhost:3001/users (+ query params opzionali)
+    // 1. GET http://localhost:3001/authors (+ query params opzionali)
     @GetMapping("")
     public List<Author> getUser(){
         return authorsService.getAuthors();
     }
 
-    // 2. POST http://localhost:3001/users (+ body)
+    // 2. POST http://localhost:3001/authors (+ body)
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED) // <-- 201
     public Author saveAuthor(@RequestBody Author body){
         return authorsService.save(body);
     }
 
-    // 3. GET http://localhost:3001/users/:id
+    // 3. GET http://localhost:3001/authors/:id
     @GetMapping("/{id}")
     public Author findById(@PathVariable int id){
         return authorsService.findAuthorById(id);
     }
 
-    // 4. PUT http://localhost:3001/users/:id (+ body)
+    // 4. PUT http://localhost:3001/authors/:id (+ body)
     @PutMapping("/{id}")
     public Author findByIdAndUpdate(@PathVariable int id, @RequestBody Author body){
         return authorsService.findAuthorByIdAndUpdate(id, body);
     }
 
-    // 5. DELETE http://localhost:3001/users/:id
+    // 5. DELETE http://localhost:3001/authors/:id
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT) // <-- 204 NO CONTENT
     public void findByIdAndDelete(@PathVariable int id){
--- src/main/java/massimomauro/S6L2RESTfulAPISpringWeb/controllers/BlogsController.java
+++ src/main/java/massimomauro/S6L2RESTfulAPISpringWeb/controllers/BlogsController.java
@@ -1,4 +1,50 @@
 package massimomauro.S6L2RESTfulAPISpringWeb.controllers;
 
+import massimomauro.S6L2RESTfulAPISpringWeb.entities.Author;
+import massimomauro.S6L2RESTfulAPISpringWeb.entities.Blog;
+import massimomauro.S6L2RESTfulAPISpringWeb.services.AuthorsService;
+import massimomauro.S6L2RESTfulAPISpringWeb.services.BlogsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/blogs")
 public class BlogsController {
+    @Autowired
+    private BlogsService blogsService;
+
+    // 1. GET http://localhost:3001/blogs (+ query params opzionali)
+    @GetMapping("")
+    public List<Blog> getUser(){
+        return blogsService.getBlogs();
+    }
+
+    // 2. POST http://localhost:3001/blogs (+ body)
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED) // <-- 201
+    public Blog saveBlog(@RequestBody Blog body){
+        return blogsService.save(body);
+    }
+
+    // 3. GET http://localhost:3001/blogs/:id
+    @GetMapping("/{id}")
+    public Blog findById(@PathVariable int id){
+        return blogsService.findBlogById(id);
+    }
+
+    // 4. PUT http://localhost:3001/blogs/:id (+ body)
+    @PutMapping("/{id}")
+    public Blog findByIdAndUpdate(@PathVariable int id, @RequestBody Blog body){
+        return blogsService.findBlogByIdAndUpdate(id, body);
+    }
+
+    // 5. DELETE http://localhost:3001/blogs/:id
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // <-- 204 NO CONTENT
+    public void findByIdAndDelete(@PathVariable int id){
+        blogsService.findBlogByIdAndDelete(id);
+    }
 }
